d35ea80fd8711b04db6e4d9ea9677dcffd30ed1b
Mateus-proc <mateusrocha.pereira2@gmail.com>
2022-10-13 10:21:12 -0300

    Add Classes modelo -Foi criado classes as classes modelos para o sistema

---
+++ src/main/java/domain/entity/Produto.java
@@ -0,0 +1,34 @@
+package domain.entity;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    Integer id;
+    String descricao;
+    private BigDecimal preco;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
