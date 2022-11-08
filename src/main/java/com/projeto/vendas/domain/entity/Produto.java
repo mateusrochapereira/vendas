82b681a8dff5977f241e3d3a1c8058f9ade51f9d
Mateus <mateusrocha.pereira2@gmail.com>
2022-11-07 21:20:03 -0300

    Mapeando enitdade produto

--- src/main/java/com/projeto/vendas/domain/entity/Produto.java
+++ src/main/java/com/projeto/vendas/domain/entity/Produto.java
@@ -1,11 +1,21 @@
 package com.projeto.vendas.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name= "produto")
 public class Produto {
 
-    Integer id;
-    String descricao;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
