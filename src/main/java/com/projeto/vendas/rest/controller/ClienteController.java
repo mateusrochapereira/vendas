92c05f7ffab44ffcd4eae502f3555a22c0108531
Mateus <mateusrocha.pereira2@gmail.com>
2022-11-14 11:24:48 -0300

    Requsisicao GET com parâmetros e Responde Entity
    -foi feito uma requisicao de buscar por id com parametros usando o Response Entity

--- src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
@@ -1,22 +1,36 @@
 package com.projeto.vendas.rest.controller;
 
 import com.projeto.vendas.domain.entity.Cliente;
+import com.projeto.vendas.domain.repository.ClientesRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @Controller
 public class ClienteController {
 
-    @RequestMapping(
-            value = {"api/clientes/hello/{nome}", "/api/hello"},
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"}
-    )
-    @ResponseBody
-    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, ResponseBody Cliente cliente){
-        return String.format("Hello %s ", nomeCliente);
+    private ClientesRepository clientesRepository;
+
+    public ClienteController(ClientesRepository clientesRepository) {
+        this.clientesRepository = clientesRepository;
     }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientesRepository.findById(id);
+
+        if(cliente.isPresent()){
+            return  ResponseEntity.ok(cliente.get());
+        }
+            return ResponseEntity.notFound().build();
+
+
+    }
+
+
 
 }
