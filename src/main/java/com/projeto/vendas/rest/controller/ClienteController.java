c08f5a0055a0ed0afa6a09ddce31155a537dfbd6
Mateus <mateusrocha.pereira2@gmail.com>
2022-11-14 10:40:23 -0300

    Mapeando requisições com Request Mapping
    -Foi feito exemplo de configurações no requeste mapping, como value, method,consumes,produces, etc

--- src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
@@ -1,16 +1,21 @@
 package com.projeto.vendas.rest.controller;
 
+import com.projeto.vendas.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value= "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"api/clientes/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"},
+            produces = {"application/json", "application/xml"}
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, ResponseBody Cliente cliente){
         return String.format("Hello %s ", nomeCliente);
     }
 
