58b39171cc0d968e06b7852123e06fb164458e3e
Mateus <mateusrocha.pereira2@gmail.com>
2022-11-15 11:03:17 -0300

    Update cliente -Foi feito um metódo de update para o cliente

--- src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/projeto/vendas/rest/controller/ClienteController.java
@@ -18,7 +18,7 @@ public class ClienteController {
         this.clientesRepository = clientesRepository;
     }
 
-    @GetMapping("/api/clientes/{id}")
+    @GetMapping("/api/clientePorId/{id}")
     @ResponseBody
     public ResponseEntity getClienteById(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientesRepository.findById(id);
@@ -31,11 +31,37 @@ public class ClienteController {
 
     @PostMapping("/api/salvarCliente")
     @ResponseBody
-        public ResponseEntity save(@RequestBody  Cliente cliente){
+    public ResponseEntity save(@RequestBody Cliente cliente) {
 
         Cliente clienteSalvo = clientesRepository.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
 
     }
 
+    @DeleteMapping("/api/deletarCliente/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientesRepository.findById(id);
+
+        if (cliente.isPresent()) {
+            clientesRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/api/atualizarCliente/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id,
+                                 @RequestBody Cliente cliente) {
+//é uma forma de usar um update com o .map
+        return clientesRepository
+                .findById(id)
+                .map( clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientesRepository.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
+
